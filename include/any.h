f1f52d6591a7e773b98b61de18a97b98b336d95d
alalazo <massimiliano.culpo@googlemail.com>
2017-06-25 14:32:06 +0200

    any: select an implementation at compile-time
    The implementation selected is one of:
    
    - std::any
    - std::experimental::any
    - boost::any
    
    Looked up in order. First found, first used.

--- include/any.h
+++ include/any.h
@@ -17,16 +17,34 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef UNIVERSALRETRIEVER_ANY_H_20170625
-#define UNIVERSALRETRIEVER_ANY_H_20170625
+#ifndef ANY_H_20170625
+#define ANY_H_20170625
 
+#if __has_include(<any>) && __cplusplus > 201402L
+#include <any>
+#elif __has_include(<experimental/any>) && __cplusplus > 201402L
 #include <experimental/any>
+#elif __has_include(<boost/any.hpp>)
+#include <boost/any.hpp>
+#else
+#error "Found no implementation of 'any' class."
+#endif
 
 namespace universal_retriever {
+#if __has_include(<any>) && __cplusplus > 201402L
+    using std::any;
+    using std::any_cast;
+    using std::bad_any_cast;
+#elif __has_include(<experimental/any>) && __cplusplus > 201402L
     using std::experimental::any;
     using std::experimental::any_cast;
     using std::experimental::bad_any_cast;
+#elif __has_include(<boost/any.hpp>)
+    using boost::any;
+    using boost::any_cast;
+    using boost::bad_any_cast;
+#endif
 }
 
 
-#endif //UNIVERSALRETRIEVER_ANY_H_20170625
+#endif //ANY_H_20170625
